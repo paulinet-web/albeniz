7e8b6e5dcd5e1c41e7f8ae70f92d6ebbf0b6be98
Pauline Turk <paulinet@theodo.fr>
2022-12-16 14:54:02 +0100

    project initialisation

---
+++ src/main/java/com/theodo/albeniz/AlbenizApplication.java
@@ -0,0 +1,13 @@
+package com.theodo.albeniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbenizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbenizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theodo/albeniz/AlbenizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theodo.albeniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbenizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
