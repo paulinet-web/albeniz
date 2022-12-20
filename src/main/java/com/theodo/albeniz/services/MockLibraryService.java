08f6fc3127eafe84ebdd7c9e00c0ddd15b02e132
Pauline Turk <paulinet@theodo.fr>
2022-12-20 12:20:15 +0100

    feat: add interface + implementation mock and data

---
+++ src/main/java/com/theodo/albeniz/services/MockLibraryService.java
@@ -0,0 +1,43 @@
+package com.theodo.albeniz.services;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.theodo.albeniz.model.Tune;
+
+@Service
+@Profile("mock")
+public class MockLibraryService implements LibraryService {
+    
+
+    public final static Map<Integer, Tune> LIBRARY = new HashMap<>();
+        
+    static {
+            LIBRARY.put(1, new Tune(1, "You belong with me", "Taylor Swift"));
+            LIBRARY.put(2, new Tune(2, "Girls just want to have fun", "Cyndi Lauper"));
+            LIBRARY.put(3, new Tune(3, "Why not me?", "The Judds"));
+        }
+
+    
+    @Override
+    public Collection<Tune> getAll(String query) {
+        return LIBRARY
+                .values()  // ne prendre que les values et pas les keys de la map
+                .stream()
+                .sorted(Comparator.comparing(Tune::getId))    // sort unsorted stream by IDs
+                .filter(tune -> query == null || tune.getTitle().toUpperCase().contains(query.toUpperCase()))   // if query == null, ne filtre pas --> prend tous les tunes
+                .collect(Collectors.toList());
+        }
+
+    @Override
+    public Tune getOne(int id) {
+        return LIBRARY.get(id);
+        }
+        
+}
--- src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
+++ src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(profiles = "mock")
 public class LibraryControllerTest {
 
     @Autowired
