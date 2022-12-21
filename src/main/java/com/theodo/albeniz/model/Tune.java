6fdcedb14f3194fa3b8ebbd40cce1313db011155
Pauline Turk <paulinet@theodo.fr>
2022-12-21 12:57:54 +0100

    feat: add valid title

--- src/main/java/com/theodo/albeniz/model/Tune.java
+++ src/main/java/com/theodo/albeniz/model/Tune.java
@@ -1,5 +1,6 @@
 package com.theodo.albeniz.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +10,7 @@ public class Tune {
 
     @Getter @Setter
     private int id;
-    @Getter @Setter
+    @Getter @Setter @NotBlank(message = "title is mandatory")
     private String title;
     @Getter @Setter
     private String author;
